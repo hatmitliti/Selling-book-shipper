e9854e0b8b45f49777103b10c7ec85eeba0f8254
Trần Quốc Trị <tritran1803@gmail.com>
2021-11-06 22:12:06 +0700

    update function change pass

--- app/src/main/java/com/example/book/Screen/ScreenDelivered.java
+++ app/src/main/java/com/example/book/Screen/ScreenDelivered.java
@@ -31,7 +31,7 @@ public class ScreenDelivered extends AppCompatActivity {
     RecyclerView recyclerView;
     ArrayList<Bill> deliveredArrayList = new ArrayList<>();
     ArrayList<String> mKey = new ArrayList<>();
-
+ 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
